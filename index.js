36056aab927d846e8b7af6803f97ffbc36061d87
benson <bensonmunge545@gmail.com>
2025-03-08 06:59:31 +0300

    added new changes

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  // Test the functions
+console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("paint")); // "This Saturday, I want to paint!"
+
+console.log(mondayWork()); // "This Monday, I will go to the office."
+console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+
+const encouragingPromptFunction = wrapAdjective("%");
+console.log(encouragingPromptFunction("a dedicated programmer")); // "You are %a dedicated programmer%!"
+
